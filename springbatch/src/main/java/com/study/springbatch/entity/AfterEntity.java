8b8b8cc15a56e80b611cd8dd0144adcd24e0e2df
신영한 <syhan7516@naver.com>
2024-07-30 20:48:13 +0900

    엔티티 기본 생성자 추가

--- springbatch/src/main/java/com/study/springbatch/entity/AfterEntity.java
+++ springbatch/src/main/java/com/study/springbatch/entity/AfterEntity.java
@@ -1,14 +1,14 @@
 package com.study.springbatch.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AfterEntity {
 
     @Id
--- springbatch/src/main/java/com/study/springbatch/entity/BeforeEntity.java
+++ springbatch/src/main/java/com/study/springbatch/entity/BeforeEntity.java
@@ -4,11 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BeforeEntity {
 
     @Id
